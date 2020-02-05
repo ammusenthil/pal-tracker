e52967772e2b2a8c574d8393f453e94f9bd3261b
Bill Kable <bkable@gmail.com>
2020-02-05 00:41:11 -0600

    test

--- src/test/java/test/pivotal/pal/tracker/JdbcTimeEntryRepositoryTest.java
+++ src/test/java/test/pivotal/pal/tracker/JdbcTimeEntryRepositoryTest.java
@@ -28,6 +28,12 @@ public class JdbcTimeEntryRepositoryTest {
 
         subject = new JdbcTimeEntryRepository(dataSource);
 
+<<<<<<< HEAD
+=======
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.execute("DELETE FROM time_entries");
+
+>>>>>>> 1511056... Add task for migrating databases
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 
@@ -36,7 +42,11 @@ public class JdbcTimeEntryRepositoryTest {
         TimeEntry newTimeEntry = new TimeEntry(123, 321, LocalDate.parse("2017-01-09"), 8);
         TimeEntry entry = subject.create(newTimeEntry);
 
+<<<<<<< HEAD
         Map<String, Object> foundEntry = jdbcTemplate.queryForMap("Select * from time_entries where id = 1", entry.getId());
+=======
+        Map<String, Object> foundEntry = jdbcTemplate.queryForMap("Select * from time_entries where id = ?", entry.getId());
+>>>>>>> 1511056... Add task for migrating databases
 
         assertThat(foundEntry.get("id")).isEqualTo(entry.getId());
         assertThat(foundEntry.get("project_id")).isEqualTo(123L);
